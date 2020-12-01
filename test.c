1270c0e78783f9881a08a6489577445e84e6aadd
李耀辉 <1263903931@qq.com>
2020-12-01 18:31:04 +0800

    9*9multiplication table

---
+++ test.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+#include<Windows.h>
+#pragma warning (disable:4996)
+
+int main(){
+	for (int i = 1; i < 10; i++){
+		for (int x = 1; x <= i; x++){
+			int result = x*i;
+			printf("%d*%d=%d ", x, i, result);
+			if (x == i){
+				printf("\n");
+			}
+		}
+	}
+	system("pause");
+	return 0;
+}
